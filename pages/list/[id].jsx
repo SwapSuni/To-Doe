84ae13dd48349763bdbf6d2620ee7918e05f21ea
Swapnil <swapnildd1702@gmail.com>
2023-12-30 22:29:52 +0530

    Updated code

--- pages/list/[id].jsx
+++ pages/list/[id].jsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import Head from 'next/head';
 
-const ist = ({ tasks }) => {
+const List = ({ tasks }) => {
     const [add, setAdd] = useState(false);
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
@@ -116,4 +116,4 @@ export async function getServerSideProps(ctx) {
     }
 }
 
-export default list
+export default List
